5900144d5078f32cf38637de485d3022028a1c49
Timur Vafin <timur.vafin@flatstack.com>
2016-04-08 12:45:09 +0300

    Add config.cson

--- atom-bootstrap.sh
+++ atom-bootstrap.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+ATOM_CONFIG_DIR="$HOME/.atom"
+
+# install atom
 brew cask install atom
+
+# install esential packadges
 apm install highlight-selected language-slim linter linter-ruby language-elixir advanced-open-file
+
+# install configuration
+curl --silent --show-error --fail --location \
+    'https://raw.githubusercontent.com/fs/atom-bootstrap/master/config.cson' \
+    --output "${ATOM_CONFIG_DIR}/config.cson"
